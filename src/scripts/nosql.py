fb40cab1afe9cf4f12ca883f5bea63e336912294
Shi Min <shiminlove20@gmail.com>
2025-06-27 21:12:45 +0800

    feature(../recipe_filter2.py): GenAI code to achieve close to desired outcome.

--- src/scripts/nosql.py
+++ src/scripts/nosql.py
@@ -1,1 +1,13 @@
-import pymongo
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+
+# Database name
+db = client["allergen-free-recipes"]
+
+# Collections
+recipes = db["Recipes"]
+reviews = db["Reviews"]
+recipeIngr = db["RecipeIngredient"]
+
+# Datas
